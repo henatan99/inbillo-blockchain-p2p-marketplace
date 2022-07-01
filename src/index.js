c11092b2b59a23fa01db7e0e8239c9e40d034214
Jim Kratus <jimkratus9@gmail.com>
2022-07-01 16:15:30 -0400

    Add index, foregn keys and constraints to accounts table

--- src/index.js
+++ src/index.js
@@ -21,11 +21,12 @@ db.connect((err) => {
 });
 
 const app = express();
+app.use(express.json());
 
 // create DB
 app.get('/', (req, res) => {
     console.log('Hi there');
-})
+});
 
 app.get('/createdb', (req, res) => {
     let sql = 'CREATE DATABASE inbillo';
@@ -36,12 +37,60 @@ app.get('/createdb', (req, res) => {
     });
 });
 
+app.get('/alterdb', (req, res) => {
+    let sql = 'ALTER TABLE `accounts` DROP COLUMN `id BIGINT(19)`';
+    let sql2 = 'ALTER TABLE `accounts` ADD COLUMN `id BIGINT(19) NOT NULL AUTO_INCREMENT`';
+
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+
+    db.query(sql2, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+})
+// NOT NULL AUTO_INCREMENT
+
+// create user table
+app.get('/createuserstable', (req, res) => {
+    let sql = `
+    CREATE TABLE users(
+        id INT(10) NOT NULL AUTO_INCREMENT,
+        user_name VARCHAR(255),
+        address VARCHAR(255),
+        phone VARCHAR(15),
+        email VARCHAR(255),
+        inactive TINYINT(3),
+        PRIMARY KEY(id)
+    )`
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Users table created ...');
+    });
+});
+
+//drop account table
+
+app.get('/dropaccountstable', (req, res) => {
+    let sql = `
+        DROP TABLE accounts
+    `
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Accounts table dropper...');
+    })
+});
+
 // create account table
 app.get('/createaccountstable', (req, res) => {
     // let sql = 'CREATE TABLE accounts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
     let sql = `
     CREATE TABLE accounts(
-        id BIGINT(19), 
+        id BIGINT(19) NOT NULL AUTO_INCREMENT,
         account_name VARCHAR(255), 
         account_type SMALLINT(5), 
         account_no VARCHAR(20), 
@@ -49,7 +98,16 @@ app.get('/createaccountstable', (req, res) => {
         inserted_at DATETIME, 
         inserted_by VARCHAR(255), 
         updated_at DATETIME, 
-        updated_by VARCHAR(255)
+        updated_by VARCHAR(255),
+        holder_id INT(10) NOT NULL,
+        PRIMARY KEY(id),
+        INDEX(account_type),
+        CONSTRAINT UC_Account UNIQUE (account_no),
+        CONSTRAINT fk_user
+        FOREIGN KEY(holder_id)
+        REFERENCES users(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
     )`
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -64,7 +122,8 @@ app.get('/createtransactionstable', (req, res) => {
         id INT(10), 
         document_id INT(10), 
         transaction_date Date, 
-        description VARCHAR(255)
+        description VARCHAR(255),
+        PRIMARY transactions_transaction_date_idx
     )`
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -80,7 +139,11 @@ app.get('/createledgerstable', (req, res) => {
         account_id BIGINT(19), 
         entry_type ENUM('buy', 'sell', 'lend', 'borrow'), 
         amount DECIMAL(20, 2), 
-        person_id INT(10)
+        person_id INT(10),
+        PRIMARY ledgers_ledger_entry_type_idx,
+        PRIMARY ledger_entries_transaction_id_fk,
+        PRIMARY ledger_entries_account_id_fk,
+        PRIMARY ledger_entries_person_id_fk
     )`
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -102,7 +165,10 @@ app.get('/createdocumentstable', (req, res) => {
         inserted_at DATETIME, 
         inserted_by VARCHAR(255), 
         upadted_at DATETIME, 
-        updated_by VARCHAR(255)
+        updated_by VARCHAR(255),
+        PRIMARY (
+            documents_document_date_idx,
+            documents_document_type_idx,
     )`
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -111,7 +177,88 @@ app.get('/createdocumentstable', (req, res) => {
     });
 });
 
+//get users
 
+app.get('/users', (req, res) => {
+    let sql = 'SELECT * FROM users';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send(results); 
+    });
+});
+
+// create user
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+
+    let user = {
+        user_name: req.body.user_name,
+        address: req.body.address,
+        phone: req.body.phone,
+        email: req.body.email,
+        inactive: req.body.inactive,
+    };
+
+    console.log(user);
+
+    let sql = 'INSERT INTO users SET ?';
+    let query = db.query(sql, user, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Account 1 added...'); 
+    });
+});
+
+
+// Get accounts 
+app.get('/accounts', (req, res) => {
+    let sql = 'SELECT * FROM accounts';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send(results); 
+    });
+});
+
+// create account 
+
+app.post('/accounts', (req, res) => {
+    console.log(req.body);
+
+    let account = {
+        account_name: req.body.account_name, 
+        account_type: req.body.account_type, 
+        account_no: req.body.account_no, 
+        is_archived: req.body.is_archived, 
+        inserted_at: new Date(Date.now()), 
+        inserted_by: req.body.inserted_by, 
+        updated_at: new Date(Date.now()), 
+        updated_by: req.body.updated_by,
+        holder_id: req.body.holder_id
+    };
+
+    console.log(account);
+
+    let sql = 'INSERT INTO accounts SET ?';
+    let query = db.query(sql, account, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Account 1 added...'); 
+    });
+});
+
+// get account by id
+
+app.get('/accounts/:id', (req, res) => {
+    let sql = `SELECT * FROM accounts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result); 
+    });
+});
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
